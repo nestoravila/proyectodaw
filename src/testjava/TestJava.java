77b540e101224122bc85177d751e6fa8027bad39
nestoravila <nestoravilagonzalez@gmail.com>
2015-03-14 00:06:07 +0100

    Nestor: He canviado la forma de loguear  añadido hola Samuel

--- src/testjava/TestJava.java
+++ src/testjava/TestJava.java
@@ -21,7 +21,7 @@ public class TestJava {
         for(int k=2;k>0;k--){
             int resultado = c/k;
             System.out.print("Valor de c/k:"+resultado);
-         
+         //HOlaaa Samuel
           // Aqui es donde editamos el codigo de NetBeans.  
         }
     }
